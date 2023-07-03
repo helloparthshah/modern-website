7488eb32e6a4b110c72ecad8c28e25ea785d887b
Parth Shah <helloparthshah@gmail.com>
2023-07-03 07:14:34 -0700

    Added styling to topbar

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,9 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Parth Shah</title>
         <meta name="description" content="Parth Shah's personal website." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/images/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet" />
