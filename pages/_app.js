f701181abf1b554b06445b20d01ca810cf55f81e
Parth Shah <helloparthshah@gmail.com>
2023-07-03 06:34:10 -0700

    Bug fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import '@/styles/globals.css'
 import Layout from '@/components/layout';
 import Head from 'next/head';
@@ -9,6 +9,9 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Parth Shah</title>
         <meta name="description" content="Parth Shah's personal website." />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet" />
       </Head>
       <Layout>
         <Component {...pageProps} />
