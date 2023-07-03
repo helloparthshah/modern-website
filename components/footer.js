4455e21bf644bdeb4a9be4b314d941869ad780eb
Parth Shah <helloparthshah@gmail.com>
2023-07-02 23:48:51 -0700

    Updated to add citations
    Co-authored-by: kunpai <kunpai@ucdavis.edu>

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Button, Col, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
 /**
  * @component
@@ -9,15 +10,20 @@ import { Button, Col, Row } from "react-bootstrap";
  */
 export default function Footer() {
 
+  const [lastUpdated, setLastUpdated] = useState("");
+
+  useEffect(() => {
+    const date = new Date(process.env.LAST_UPDATED);
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    setLastUpdated(date.toLocaleDateString(undefined, options));
+  }, []);
+
   return (
     <footer>
       <div className="d-flex flex-column justify-content-center align-items-center bg-light mt-5">
         <Row className="h-100 w-75">
           <Col className="text-center primary d-flex flex-column h-100 pt-2 pb-2 gap-1 footer-col align-items-center">
             <span className="text-muted main-text-regular">Connect with Me</span>
-            {/* <Link href="https://scholar.google.com/citations?hl=en&user=-0ZTuE4AAAAJ">
-              Google Scholar
-            </Link> */}
             <Link href="https://github.com/helloparthshah">
               GitHub
             </Link>
@@ -30,12 +36,15 @@ export default function Footer() {
           </Col>
         </Row>
         <Row className="w-100">
-            <div className="w-100 d-flex flex-column align-items-center justify-content-center pb-3">
-                <hr className="w-100" />
-                <p className="main-text-regular text-center">
-                    This website was created by Kunal Pai, Parth Shah and Harshil Patel.
-                </p>
-            </div>
+          <div className="w-100 d-flex flex-column align-items-center justify-content-center pb-3">
+            <hr className="w-100" />
+            <p className="main-text-regular text-center">
+              This website was created by Kunal Pai, Parth Shah and Harshil Patel.
+            </p>
+            <p>
+              Last updated on {lastUpdated}
+            </p>
+          </div>
         </Row>
       </div>
     </footer>
